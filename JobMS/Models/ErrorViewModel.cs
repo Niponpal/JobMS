e092b6b0ee26c92dd522299b3fcb7af6a9d92f8e
Niponpal <niponpal52@gmail.com>
2026-04-08 00:17:44 +0600

    Add project files.

---
+++ JobMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
