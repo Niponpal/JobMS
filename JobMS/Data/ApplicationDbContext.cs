921827ec06d0c2551189415e1cb5bae653bc294d
Niponpal <niponpal52@gmail.com>
2026-04-09 10:40:08 +0600

    Fix Setup 🚀🚀🚑

--- JobMS/Data/ApplicationDbContext.cs
+++ JobMS/Data/ApplicationDbContext.cs
@@ -1,6 +1,11 @@
-﻿namespace JobMS.Data
+﻿using Microsoft.EntityFrameworkCore;
+namespace JobMS.Data;
+
+public class ApplicationDbContext : DbContext
 {
-    public class ApplicationDbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
+    public DbSet<Models.Application> Applications { get; set; }
+    public DbSet<Models.Job> Jobs { get; set; }
 }
