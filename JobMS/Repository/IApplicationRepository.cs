18eae938fb9611f0bad8ee6c0960c153d66917a3
Niponpal <niponpal52@gmail.com>
2026-04-10 00:59:21 +0600

    Fix Repository 🚀🚀🚑

---
+++ JobMS/Repository/IApplicationRepository.cs
@@ -0,0 +1,14 @@
+﻿using JobMS.Models;
+
+namespace JobMS.Repository;
+
+public interface IApplicationRepository
+{
+    // CRUD operations for Application entity
+    Task<IEnumerable<Application>> GetAllApplicationsAsync(CancellationToken cancellationToken);
+    Task<Application?> GetApplicationByIdAsync(long id, CancellationToken cancellationToken);
+    Task<Application> AddApplicationAsync(Application application, CancellationToken cancellationToken);
+    Task<Application?> UpdateApplicationAsync(Application application, CancellationToken cancellationToken);
+    Task<Application> DeleteApplicationAsync(long id, CancellationToken cancellationToken);
+  
+}
