f37dfd10a646fe03ac11aa17def3e0acf617b1f2
Niponpal <niponpal52@gmail.com>
2026-04-14 11:51:56 +0600

    Fix Applicaiton Controller 🚀🚀🚑

--- JobMS/Controllers/ApplicationController.cs
+++ JobMS/Controllers/ApplicationController.cs
@@ -1,4 +1,5 @@
-﻿using JobMS.Repository;
+﻿using JobMS.Models;
+using JobMS.Repository;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -9,13 +10,60 @@ public class ApplicationController : Controller
     private readonly IApplicationRepository _applicationRepository;
 
     public ApplicationController(IApplicationRepository applicationRepository)
-    { 
+    {
         _applicationRepository = applicationRepository;
     }
 
+    [HttpGet]
     public async Task<IActionResult> Index(CancellationToken cancellationToken)
     {
         var data = await _applicationRepository.GetAllApplicationsAsync(cancellationToken);
+        if (data == null)
+        {
+            return NotFound();
+        }
+        return View(data);
+    }
+    [HttpGet]
+    public async Task<IActionResult> CreateOrEdit(int id, CancellationToken cancellationToken)
+    {
+        if (id == 0)
+        {
+            return View(new Application()); // ✅ FIX HERE
+        }
+        var data = await _applicationRepository.GetApplicationByIdAsync(id, cancellationToken);
+        if (data == null)
+        {
+            return NotFound();
+        }
+        return View(data);
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateOrEdit(Application application, CancellationToken cancellationToken)
+    {
+
+        if (application.Id == 0)
+        {
+            await _applicationRepository.AddApplicationAsync(application, cancellationToken);
+            return RedirectToAction(nameof(Index));
+        }
+        else
+        {
+            await _applicationRepository.UpdateApplicationAsync(application, cancellationToken);
+        }
+        return RedirectToAction(nameof(Index));
+
+    }
+    [HttpPost]
+    public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken)
+    {
+        await _applicationRepository.DeleteApplicationAsync(id, cancellationToken);
+        return RedirectToAction(nameof(Index));
+    }
+    [HttpGet]
+    public async Task<IActionResult> Details(int id, CancellationToken cancellationToken)
+    {
+        var data = await _applicationRepository.GetApplicationByIdAsync(id, cancellationToken);
         if (data == null)
         {
             return NotFound();
